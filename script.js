6e1d064f8cb341879a888bf8cb71de4a184f6bac
Balveer Singh Rathore <balveersinghrathore51@gmail.com>
2024-04-08 18:09:02 +0530

    feat (HomePage): Add mobile side menubar

---
+++ script.js
@@ -0,0 +1,43 @@
+// copy menu for mobile
+function copyMenu() {
+  // COPY -inside .dpt-cat to .department
+  var dptCat = document.querySelector(".dpt-cat");
+  var department = document.querySelector(".departments");
+  department.innerHTML = dptCat.innerHTML;
+
+  // COPY -inside .nav to .nav
+  var mainNav = document.querySelector(".header-nav nav");
+  var mobileMainNav = document.querySelector(".off-canvas nav");
+  mobileMainNav.innerHTML = mainNav.innerHTML;
+
+  // COPY -inside .header-top .wrapper to .thetop-nav
+  var topNav = document.querySelector(".header-top .wrapper");
+  var mobileTopNav = document.querySelector(".off-canvas .thetop-nav");
+  mobileTopNav.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+// Show sub-menu on mobile
+const mobileMenu = document.querySelectorAll(".has-child .icon-small");
+mobileMenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  mobileMenu.forEach((menu) =>
+    menu != this ? menu.closest(".has-child").classList.remove("expand") : null
+  );
+
+  if (this.closest(".has-child").classList != "expand")
+    this.closest(".has-child").classList.toggle("expand");
+}
+
+// Show the side menu
+const startMeneBtn = document.querySelector(".trigger"),
+  closeMenuBtn = document.querySelector(".t-close"),
+  addMenuSection = document.querySelector(".site");
+startMeneBtn.addEventListener("click", () => {
+  addMenuSection.classList.toggle("show-menu");
+});
+closeMenuBtn.addEventListener("click", () => {
+  addMenuSection.classList.remove("show-menu");
+});
