5d60322fe77a24790a2be59e174c88645b58ec62
Balveer Singh Rathore <balveersinghrathore51@gmail.com>
2024-04-23 14:01:15 +0530

    feat (HomePage): Add slider to hero section

--- script.js
+++ script.js
@@ -41,3 +41,14 @@ startMeneBtn.addEventListener("click", () => {
 closeMenuBtn.addEventListener("click", () => {
   addMenuSection.classList.remove("show-menu");
 });
+
+// Slider
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
